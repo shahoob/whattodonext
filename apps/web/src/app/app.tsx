915115242b35294b9427756945a08136275a04cd
Shehab Ali Tweel <me@shehabtweel.ly>
2023-08-30 18:51:25 +0200

    chore(web): remove vitest and anything related

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -13,25 +13,3 @@ export function App() {
 }
 
 export default App;
-
-if (import.meta.vitest) {
-  // add tests related to your file here
-  // For more information please visit the Vitest docs site here: https://vitest.dev/guide/in-source.html
-
-  const { it, expect, beforeEach } = import.meta.vitest;
-  let render: any;
-
-  beforeEach(async () => {
-    render = (await import('@testing-library/react')).render;
-  });
-
-  it('should render successfully', () => {
-    const { baseElement } = render(<App />);
-    expect(baseElement).toBeTruthy();
-  });
-
-  it('should have a greeting as the title', () => {
-    const { getByText } = render(<App />);
-    expect(getByText(/Welcome web/gi)).toBeTruthy();
-  });
-}
